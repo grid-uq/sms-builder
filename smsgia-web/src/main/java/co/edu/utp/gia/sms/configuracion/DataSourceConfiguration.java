e491887081ea5946980167c78f842298f1a9967b
Christian A. Candela <christian.candela@utp.edu.co>
2020-04-03 15:07:54 -0500

    Clase de configuracion que crea Datasource a H2

---
+++ smsgia-web/src/main/java/co/edu/utp/gia/sms/configuracion/DataSourceConfiguration.java
@@ -0,0 +1,17 @@
+package co.edu.utp.gia.sms.configuracion;
+
+import javax.annotation.sql.DataSourceDefinition;
+
+@DataSourceDefinition(
+		  name = "java:app/smsgia-web/smsDS",
+		  className = "org.h2.jdbcx.JdbcDataSource",
+		  initialPoolSize = 10,
+		  minPoolSize = 10,
+		  maxPoolSize = 200,
+//		  url = "jdbc:h2:mem:test"
+		  url = "jdbc:h2:file:./sms"
+		)
+//https://blog.payara.fish/an-intro-to-connection-pools-in-payara-server-5
+public class DataSourceConfiguration {
+
+}
