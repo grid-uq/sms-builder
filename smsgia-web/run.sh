19a492297c62d624dd1754b0676cf313e150905e
luixip <luixip@192.168.1.2>
2020-04-07 20:45:34 +0200

    Funciones básicas ok

--- smsgia-web/run.sh
+++ smsgia-web/run.sh
@@ -2,3 +2,5 @@ rm -rf /Users/luixip/servers/payara5/glassfish/domains/domain1/autodeploy/*
 mvn clean 
 mvn package 
 cp target/smsgia-web.war /Users/luixip/servers/payara5/glassfish/domains/domain1/autodeploy/
+open -a safari http://localhost:8080/smsgia-web/
+
