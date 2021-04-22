d0a15f4fbd75f078dff684462efc389fe8f73bb5
Christian <christiancandela@gmail.com>
2021-04-21 21:12:54 -0500

    Se actilizan mensajes, y se incrementa la versión del proyecto y se actualizan scripts de generación de los archivos pom.

--- sms-builder-web/build-image.sh
+++ sms-builder-web/build-image.sh
@@ -1,6 +1,13 @@
 #!/bin/bash
+
+fecha=$(date '+%Y.%m.%d')
+projectVersion=$(mvn -q -Dexec.executable=echo -Dexec.args='${project.version}' --non-recursive exec:exec)
+version="$projectVersion.$fecha"
+echo $version
 docker rmi sms-builder:latest
 sh ./sms-builder-web-uberjar.sh
 docker build -t sms-builder:latest .
-docker tag sms-builder christiancandela/sms-builder
-#docker push christiancandela/sms-builder
+docker tag sms-builder griduq/sms-builder
+docker tag griduq/sms-builder griduq/sms-builder:$version
+docker push griduq/sms-builder
+docker push griduq/sms-builder:$version
