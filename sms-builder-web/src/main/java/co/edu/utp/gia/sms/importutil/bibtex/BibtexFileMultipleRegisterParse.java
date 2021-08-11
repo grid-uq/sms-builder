87705aa90a73100bf1bd248439334c808389679a
christiancandela <christiancandela@gmail.com>
2021-08-11 17:34:58 -0500

    Se continua modificando la función de importar mediante la fuente.

---
+++ sms-builder-web/src/main/java/co/edu/utp/gia/sms/importutil/bibtex/BibtexFileMultipleRegisterParse.java
@@ -0,0 +1,48 @@
+package co.edu.utp.gia.sms.importutil.bibtex;
+
+import co.edu.utp.gia.sms.entidades.Referencia;
+import co.edu.utp.gia.sms.importutil.FileMultipleRegisterParse;
+
+import java.io.InputStream;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class BibtexFileMultipleRegisterParse extends FileMultipleRegisterParse {
+
+	public BibtexFileMultipleRegisterParse(String fuente, String tipoFuente) {
+		super(new BibtexReferenceParcer(fuente,tipoFuente));
+	}
+
+	@Override
+	public List<Referencia> parse(InputStream input) {
+		List<Referencia> references = new ArrayList<>();
+
+		try (Scanner lector = new Scanner(input)) {
+
+			while (lector.hasNextLine()) {
+				String stringRerence = readReference(lector);
+				if (!"".equals(stringRerence)) {
+					Referencia reference = getReferenceParser().parse(stringRerence);
+					if (reference != null) {
+						references.add(reference);
+					}
+				}
+			}
+		}
+
+		return references;
+	}
+
+	private String readReference(Scanner lector) {
+		StringBuilder build = new StringBuilder();
+		String linea = null;
+		while (lector.hasNextLine() && !"ER  -".equals(linea)) {
+			linea = lector.nextLine().trim();
+			if (!"ER  -".equals(linea)) {
+				build.append(linea).append("\n");
+			}
+		}
+		return build.toString();
+	}
+}
