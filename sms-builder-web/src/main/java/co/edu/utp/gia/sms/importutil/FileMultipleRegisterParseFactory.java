787003c8e79d00619c48a85f0250fc87f348ce1f
Christian Andrés Candela Uribe <christiancandela@gmail.com>
2023-05-10 15:38:38 -0500

    fix: Se modifica FileMultipleRegisterParseFactory para adicionar soporte de otras fuentes.

--- sms-builder-web/src/main/java/co/edu/utp/gia/sms/importutil/FileMultipleRegisterParseFactory.java
+++ sms-builder-web/src/main/java/co/edu/utp/gia/sms/importutil/FileMultipleRegisterParseFactory.java
@@ -14,44 +14,6 @@ public class FileMultipleRegisterParseFactory {
 	
 	public static final FileMultipleRegisterParse getInstance(Fuente fuente) {
 		switch (fuente) {
-//		case ACM:
-//			return new ACMFileMultipleRegisterParse();
-//		case IEEE:
-//			return new IEEEFileMultipleRegisterParse();
-//		case SCIENCEDIRECT:
-//			return new SDFileMultipleRegisterParse();
-//		case SCOPUS:
-//			return new ScopusFileMultipleRegisterParse();
-//		case SPRINGER:
-//			return new SpringerFileMultipleRegisterParse();
-//		case WOS:
-//			return new WOSFileMultipleRegisterParse();
-		case ACM:
-			return new MendeleyRisFileMultipleRegisterParse(Fuente.ACM);
-		case IEEE:
-			return new MendeleyRisFileMultipleRegisterParse(Fuente.IEEE);
-		case SCIENCEDIRECT:
-			return new MendeleyRisFileMultipleRegisterParse(Fuente.SCIENCEDIRECT);
-		case SCOPUS:
-			return new MendeleyRisFileMultipleRegisterParse(Fuente.SCOPUS);
-		case SPRINGER:
-			return new MendeleyRisFileMultipleRegisterParse(Fuente.SPRINGER);
-		case WOS:
-			return new MendeleyRisFileMultipleRegisterParse(Fuente.WOS);
-		
-//		case ACM_MENDELEY:
-//			return new MendeleyRisFileMultipleRegisterParse(Fuente.ACM);
-//		case IEEE_MENDELEY:
-//			return new MendeleyRisFileMultipleRegisterParse(Fuente.IEEE);
-//		case SCIENCEDIRECT_MENDELEY:
-//			return new MendeleyRisFileMultipleRegisterParse(Fuente.SCIENCEDIRECT);
-//		case SCOPUS_MENDELEY:
-//			return new MendeleyRisFileMultipleRegisterParse(Fuente.SCOPUS);
-//		case SPRINGER_MENDELEY:
-//			return new MendeleyRisFileMultipleRegisterParse(Fuente.SPRINGER);
-//		case WOS_MENDELEY:
-//			return new MendeleyRisFileMultipleRegisterParse(Fuente.WOS);
-//		case MANUAL:
 		case INCLUSION_DIRECTA:
 			return new MendeleyRisFileMultipleRegisterParse(Fuente.INCLUSION_DIRECTA);
 		case SNOWBALL_BACKWARD:
@@ -59,7 +21,7 @@ public class FileMultipleRegisterParseFactory {
 		case SNOWBALL_FORWARD:
 			return new MendeleyRisFileMultipleRegisterParse(Fuente.SNOWBALL_FORWARD);
 		default:
-			return null;
+			return new MendeleyRisFileMultipleRegisterParse(fuente);
 		}
 	}
 }
