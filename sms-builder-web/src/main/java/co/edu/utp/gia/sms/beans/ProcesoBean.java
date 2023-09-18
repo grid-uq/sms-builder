bf6075bcb468f37bfd61d430976d3444dd65f401
Christian Andrés Candela Uribe <christiancandela@gmail.com>
2023-09-18 13:47:18 -0500

    fea: mejora menú del panel de control. se inicia versión 4.1

--- sms-builder-web/src/main/java/co/edu/utp/gia/sms/beans/ProcesoBean.java
+++ sms-builder-web/src/main/java/co/edu/utp/gia/sms/beans/ProcesoBean.java
@@ -8,12 +8,11 @@ import jakarta.faces.view.ViewScoped;
 import jakarta.inject.Inject;
 import jakarta.inject.Named;
 import lombok.Getter;
-import org.primefaces.model.menu.DefaultMenuItem;
-import org.primefaces.model.menu.DefaultMenuModel;
-import org.primefaces.model.menu.DefaultSubMenu;
-import org.primefaces.model.menu.MenuModel;
+import org.primefaces.model.menu.*;
 
 import java.util.Collection;
+import java.util.List;
+
 /**
  * Clase controladora de interfaz web que se encarga de la gestión del proceso.
  *
@@ -46,40 +45,49 @@ public class ProcesoBean extends AbstractRevisionBean {
 
     public void inicializar() {
         pasosProceso = procesoService.get();
-        configurarMenu();
+        stepsModel = new DefaultMenuModel();
+        model = new DefaultMenuModel();
+
+        configurarPasoActual();
+        configurarStage1(model);
+//        configurarStage2Static(model);
+//        configurarStage2(model);
         configurarStepsMenu();
+
+        configurarStage3(model);
+        configurarStage4(model);
+        configurarStage5(model);
+        configurarStage6(model);
+        configurarAyuda(model);
+
     }
 
     private void configurarStepsMenu() {
-        stepsModel = new DefaultMenuModel();
-        getRevision().getPasosProceso().forEach( this::addStep );
+        DefaultSubMenu stage = DefaultSubMenu.builder()
+                .label("Stage 2 Search for studies")
+                .styleClass(".ui-menu-parent")
+                .style("overflow-y: auto; max-height: 60vh;")
+                .build();
 
+        getRevision().getPasosProceso().stream().map( this::createStep).forEach(
+                (item)->{
+                    stepsModel.getElements().add(item);
+                    stage.getElements().add(item);
+                }
+        );
 
+        model.getElements().add(stage);
     }
 
-    private void addStep(PasoProceso pasoProceso) {
+    private MenuItem createStep(PasoProceso pasoProceso) {
         var paso = pasoProceso.getPaso();
-        var item = DefaultMenuItem.builder()
+        return DefaultMenuItem.builder()
                 .value(getMessage(paso.nombre()))
                 //.url(paso.recurso().getUrl())
                 .command("#{revisionBean.irPaso("+pasoProceso.getOrden()+")}")
                 .style("margin-left:10px; margin-right:10px; border-radius: 10px; overflow-x: auto; max-width: 90hw;")
 
                 .build();
-        stepsModel.getElements().add(item);
-    }
-
-    private void configurarMenu() {
-        model = new DefaultMenuModel();
-        configurarPasoActual();
-        configurarStage1(model);
-//        configurarStage2Static(model);
-        configurarStage2(model);
-        configurarStage3(model);
-        configurarStage4(model);
-        configurarStage5(model);
-        configurarStage6(model);
-        configurarAyuda(model);
     }
 
     private void configurarPasoActual() {
@@ -91,151 +99,90 @@ public class ProcesoBean extends AbstractRevisionBean {
         model.getElements().add(item);
     }
 
-    private void configurarStage2(MenuModel model) {
-        DefaultSubMenu stage = DefaultSubMenu.builder()
-                .label("Stage 2 Search for studies")
-                .styleClass(".ui-menu-parent")
-                .style("overflow-y: auto; max-height: 60vh;")
-                .build();
-        pasosProceso.forEach(paso -> {
-            final String url = paso.getPaso().recurso().getUrl();
-//            DefaultMenuItem menuItem = addItem(stage, paso.getPaso().getNombre(), url);
-            DefaultMenuItem menuItem= addItem(stage, getMessage(paso.getPaso().nombre()), url,
-                    getRevision() != null&&seguridadBean.verifivarAcceso(url));
-//            menuItem.setParam("paso", paso.getId());
-
-        });
-        stage.setExpanded(false);
-        model.getElements().add(stage);
-    }
-
-    private void configurarStage2Static(MenuModel model) {
-        String[] labels = {"etiquetaMenuReferenciasImportar", "etiquetaMenuReferenciasDuplicadas",
-                "etiquetaMenuReferenciasSeleccionar", "etiquetaMenuReferenciasSeleccionadas"};
-        String[] urls = {"/atributocalidad/registro.xhtml", "/revision/gestionarReferenciasRepetidas.xhtml",
-                "/revision/aplicarCriterios.xhtml", "/revision/resumenReferenciasSeleccionadas.xhtml"};
-        DefaultSubMenu stage = DefaultSubMenu.builder()
-                .label("Stage 2 Search for studies")
-                .build();
-        for (int i = 0; i < urls.length; i++) {
-            addItem(stage, getMessage(labels[i]), urls[i]);
-        }
-        stage.setExpanded(false);
-        model.getElements().add(stage);
-    }
-
     private void configurarStage1(MenuModel model) {
-        String[] urls = {"/referencia/navegar.xhtml","/referencia/navegar.xhtml?all=true","/proceso/registro.xhtml","/backup/importar.xhtml"};
-        String[] labels = {"etiquetaMenuReferenciasSeleccionadas","etiquetaMenuReferencias","etiquetaProceso","etiquetaRestaurar"};
-        DefaultSubMenu stage = DefaultSubMenu.builder()
-                .label("Stage 1 Planing")
-                .build();
-        addItem(stage, getMessage(labels[0]), urls[0], seguridadBean.verifivarAcceso(urls[0]));
-        for (int i = 1; i < urls.length; i++) {
-            addItem(stage, getMessage(labels[i]), urls[i]);
-        }
+        DefaultSubMenu stage = DefaultSubMenu.builder().label("Stage 1 Planing").build();
+
+        var items = List.of( itemOf("etiquetaMenuReferenciasSeleccionadas","/referencia/navegar.xhtml"),
+                itemOf("etiquetaMenuReferencias","/referencia/navegar.xhtml?all=true"),
+                itemOf("etiquetaProceso","/proceso/registro.xhtml"),
+                itemOf("etiquetaRestaurar","/backup/importar.xhtml"));
+        items.stream().map(this::createItem).forEach(stage.getElements()::add);
         stage.getElements().add(itemBackup());
         stage.setExpanded(false);
+
         model.getElements().add(stage);
     }
 
     private void configurarStage3(MenuModel model) {
-        String[] urls = {"/referencia/adicionarCitas.xhtml", "/calidad/evaluarReferencias.xhtml",
-                "/calidad/resumenEvaluacionReferencias.xhtml", "/calidad/resumenEvaluacionReferenciasAtributo.xhtml"};
-        String[] labels = {"etiquetaMenuReferenciaNumeroCitas", "etiquetaMenuEvaluarReferencia", "etiquetaMenuCalidadTablaResumen",
-                "etiquetaMenuCalidadTablaResumenAtributo"};
-        DefaultSubMenu stage = DefaultSubMenu.builder()
-                .label("Stage 3 Quality analisys")
-                .build();
-        for (int i = 0; i < urls.length; i++) {
-            addItem(stage, getMessage(labels[i]), urls[i]);
-        }
+        DefaultSubMenu stage = DefaultSubMenu.builder().label("Stage 3 Quality analisys").build();
+
+        var items = List.of( itemOf("etiquetaMenuCalidadTablaResumen","/calidad/resumenEvaluacionReferencias.xhtml"),
+                itemOf("etiquetaMenuCalidadTablaResumenAtributo","/calidad/resumenEvaluacionReferenciasAtributo.xhtml"));
+        items.stream().map(this::createItem).forEach(stage.getElements()::add);
         stage.setExpanded(false);
         model.getElements().add(stage);
     }
 
     private void configurarStage4(MenuModel model) {
-        String[] urls = {"/estadisticas/palabrasClave.xhtml", "/estadisticas/referenciaPalabrasClave.xhtml"};
-        String[] labels = {"etiquetaMenuPalabrasClave", "etiquetaMenuMetadato"};
-        DefaultSubMenu stage = DefaultSubMenu.builder()
-                .label("Stage 4 Data collection")
-                .build();
-        for (int i = 0; i < urls.length; i++) {
-            addItem(stage, getMessage(labels[i]), urls[i]);
-        }
+        DefaultSubMenu stage = DefaultSubMenu.builder().label("Stage 4 Data collection").build();
+
+        var items = List.of( itemOf("etiquetaMenuPalabrasClave","/estadisticas/palabrasClave.xhtml"),
+                itemOf("etiquetaMenuMetadato","/estadisticas/referenciaPalabrasClave.xhtml"));
+        items.stream().map(this::createItem).forEach(stage.getElements()::add);
         stage.setExpanded(false);
         model.getElements().add(stage);
     }
 
     private void configurarStage5(MenuModel model) {
-        String[] urls = {"/referencia/analizar.xhtml", "/estadisticas/tablaReferenciasPreguntas.xhtml",
-                "/estadisticas/tablaReferenciasTopicos.xhtml"};
-        String[] labels = {"etiquetaMenuAnalizarReferencias", "etiquetaMenuAnalisisTablaReferenciaPregunta",
-                "etiquetaMenuAnalisisTablaReferenciaTopico"};
-        DefaultSubMenu stage = DefaultSubMenu.builder()
-                .label("Stage 5 Analysis and classification of studies")
-                .build();
-        for (int i = 0; i < urls.length; i++) {
-            addItem(stage, getMessage(labels[i]), urls[i]);
-        }
+        DefaultSubMenu stage = DefaultSubMenu.builder().label("Stage 5 Analysis and classification of studies").build();
+
+        var items = List.of(
+                itemOf("etiquetaMenuAnalisisTablaReferenciaPregunta","/estadisticas/tablaReferenciasPreguntas.xhtml"),
+                itemOf("etiquetaMenuAnalisisTablaReferenciaTopico","/estadisticas/tablaReferenciasTopicos.xhtml"));
+
+
+        items.stream().map(this::createItem).forEach(stage.getElements()::add);
         stage.setExpanded(false);
         model.getElements().add(stage);
     }
 
     private void configurarStage6(MenuModel model) {
-        String[] urls = {"/estadisticas/referenciasYear.xhtml", "/estadisticas/referenciasTipo.xhtml",
-                "/estadisticas/referenciasTipoFuente.xhtml", "/estadisticas/referenciasTopico.xhtml",
-                "/estadisticas/referenciasTopicoAtributoCalidad.xhtml", "/estadisticas/referenciasPregunta.xhtml",
-                "/estadisticas/referenciasCalidadYear.xhtml","/estadisticas/referenciasTermino.xhtml"};
-        String[] labels = {"etiquetaMenuAnalisisReferenciaYear", "etiquetaMenuAnalisisReferenciaTipo",
-                "etiquetaMenuAnalisisReferenciaTipoFuente", "etiquetaMenuAnalisisReferenciaTopico",
-                "etiquetaMenuAnalisisReferenciaTopicoAtributoCalidad", "etiquetaMenuAnalisisReferenciaPregunta",
-                "etiquetaMenuAnalisisCalidadYear","etiquetaMenuAnalisisReferenciaTermino"};
-        DefaultSubMenu stage = DefaultSubMenu.builder()
-                .label("Stage 6 Results")
-                .build();
-        for (int i = 0; i < urls.length; i++) {
-            addItem(stage, getMessage(labels[i]), urls[i]);
-        }
-        stage.setExpanded(false);
-        model.getElements().add(stage);
-    }
+        DefaultSubMenu stage = DefaultSubMenu.builder().label("Stage 6 Results").build();
 
-    private void configurarGestionas(MenuModel model) {
-        String[] urls = {"/usuario/index.xhtml", "/seguridad/recurso/index.xhtml", "/seguridad/rol/index.xhtml",
-                "/seguridad/usuario/actualizar.xhtml", "/seguridad/usuario/index.xhtml"};
-        String[] labels = {"menu.usuarios", "menu.recurso", "menu.rol", "menu.usuario.actualizar", "menu.usuario.registrar"};
-        DefaultSubMenu stage = DefaultSubMenu.builder()
-                .label(getMessage("menu.gestionar"))
-                .build();
-        for (int i = 0; i < urls.length; i++) {
-            addItem(stage, getMessage(labels[i]), urls[i], seguridadBean.verifivarAcceso(urls[i]));
-        }
+        var items = List.of( itemOf("etiquetaMenuAnalisisReferenciaYear","/estadisticas/referenciasYear.xhtml"),
+                itemOf("etiquetaMenuAnalisisReferenciaTipo","/estadisticas/referenciasTipo.xhtml"),
+                itemOf("etiquetaMenuAnalisisReferenciaTipoFuente","/estadisticas/referenciasTipoFuente.xhtml"),
+                itemOf("etiquetaMenuAnalisisReferenciaTopico","/estadisticas/referenciasTopico.xhtml"),
+                itemOf("etiquetaMenuAnalisisReferenciaTopicoAtributoCalidad","/estadisticas/referenciasTopicoAtributoCalidad.xhtml"),
+                itemOf("etiquetaMenuAnalisisReferenciaPregunta","/estadisticas/referenciasPregunta.xhtml"),
+                itemOf("etiquetaMenuAnalisisCalidadYear","/estadisticas/referenciasCalidadYear.xhtml"),
+                itemOf("etiquetaMenuAnalisisReferenciaTermino","/estadisticas/referenciasTermino.xhtml")
+                );
+        items.stream().map(this::createItem).forEach(stage.getElements()::add);
         stage.setExpanded(false);
         model.getElements().add(stage);
     }
 
     private void configurarAyuda(MenuModel model) {
-        String[] urls = {"/ayuda/proceso.xhtml"};
-        String[] labels = {"menu.ayuda.proceso"};
-        DefaultSubMenu stage = DefaultSubMenu.builder()
-                .label(getMessage("menu.ayuda"))
-                .build();
-        for (int i = 0; i < urls.length; i++) {
-            addItem(stage, getMessage(labels[i]), urls[i], seguridadBean.verifivarAcceso(urls[i]));
-        }
+        DefaultSubMenu stage = DefaultSubMenu.builder().label(getMessage("menu.ayuda")).build();
+        var items = List.of(itemOf("menu.ayuda.proceso","/ayuda/proceso.xhtml"));
+
+        items.stream().map(this::createItem).forEach(stage.getElements()::add);
         stage.setExpanded(false);
 
         model.getElements().add(stage);
     }
 
-    private void addItem(DefaultSubMenu stage, String label, String url) {
-        addItem(stage, label, url, getRevision() != null && seguridadBean.verifivarAcceso(url));
+    private MenuItem createItem(Item item) {
+        return createItem(item.etiqueta(),item.path());
     }
 
-    private DefaultMenuItem addItem(DefaultSubMenu stage, String label, String url, boolean rendered) {
+    private MenuItem createItem(String label, String url) {
+        return createItem(label, url, getRevision() != null && seguridadBean.verifivarAcceso(url));
+    }
 
-        DefaultMenuItem item = DefaultMenuItem.builder()
+    private MenuItem createItem(String label, String url, boolean rendered) {
+        return DefaultMenuItem.builder()
                 .value(label)
                 .ajax(false)
                 .url(url)
@@ -244,11 +191,9 @@ public class ProcesoBean extends AbstractRevisionBean {
                 .style("overflow-y: auto; max-height: 60vh;")
                 .rendered(rendered)
                 .build();
-        stage.getElements().add(item);
-        return item;
     }
 
-    private DefaultMenuItem itemBackup(){
+    private MenuItem itemBackup(){
         return DefaultMenuItem.builder()
                 .value(getMessage("etiquetaBackup"))
                 .ajax(false)
@@ -260,4 +205,10 @@ public class ProcesoBean extends AbstractRevisionBean {
                 .build();
     }
 
+    private Item itemOf(String etiqueta,String path){
+        return new Item(getMessage(etiqueta),path);
+    }
+
+    record Item(String etiqueta,String path){
+    }
 }
